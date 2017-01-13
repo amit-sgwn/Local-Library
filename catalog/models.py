a0b0c9c772259385a02342c7fd080816cf40c826
amit-sgwn <amitsangwan898@gmail.com>
2017-01-13 13:17:13 +0530

    Added genre model

--- catalog/models.py
+++ catalog/models.py
@@ -1,28 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class MyModelName(models.Model):
+class Genre(models.Model):
     """
-    A typical class defining a model, derived from the Model class.
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
-
-    # Fields
-    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
-    ...
-
-    # Metadata
-    class Meta:
-        ordering = ["-my_field_name"]
-
-    # Methods
-    def get_absolute_url(self):
-         """
-         Returns the url to access a particular instance of MyModelName.
-         """
-         return reverse('model-detail-view', args=[str(self.id)])
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
 
     def __str__(self):
         """
-        String for representing the MyModelName object (in Admin site etc.)
+        String for representing the Model object (in Admin site etc.)
         """
-        return self.field_name
+        return self.name
