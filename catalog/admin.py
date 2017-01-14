70f623e59ac6b317def6765ab7f12c4cadcba8af
amit-sgwn <amitsangwan898@gmail.com>
2017-01-14 16:24:48 +0530

    Added more functionality to Admin.py at BookInstanceAdmin class to show due date of book at admin display

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,7 +13,7 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
 # Register your models here.
 #admin.site.register(Book)
 #admin.site.register(Author,AuthorAdmin)
