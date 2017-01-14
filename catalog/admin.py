d99a13e24a0a8fdda2e4a4fea96b9b620a8b2a56
amit-sgwn <amitsangwan898@gmail.com>
2017-01-14 15:59:35 +0530

    Added admin class for all model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(Book)
+#admin.site.register(Author,AuthorAdmin)
+#admin.site.register(Genre)
+#admin.site.register(BookInstance)
