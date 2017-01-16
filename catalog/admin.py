94ead9ead0eedfcb234cf02276f52eb8a6b16811
amit-sgwn <amitsangwan898@gmail.com>
2017-01-16 14:49:26 +0530

    Added inline editing for Author admin to show all his written books

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BookInline(admin.TabularInline):
+    model = Book
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-class BooksInstanceInline(admin.TabularInline):
-    model = BookInstance
+    inlines = [BookInline]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
