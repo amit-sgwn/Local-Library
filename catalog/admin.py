1ffe6627eeb28615bd2439a0c5b4c4024e1585ce
amit-sgwn <amitsangwan898@gmail.com>
2017-01-16 14:13:45 +0530

    Added new feature to create section in book instance section using fieldsets

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,7 @@ from .models import Author, Genre, Book, BookInstance
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -14,6 +15,14 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 # Register your models here.
 #admin.site.register(Book)
 #admin.site.register(Author,AuthorAdmin)
